d26be19685b12518db8025182eea309aba7d1791
arne-afk <79860751+arne-afk@users.noreply.github.com>
2021-06-09 10:20:12 +0300

    added other pages

---
+++ js/navbar.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+    const menu = document.querySelector('.menu');
+    const mobile_navbar_div = document.querySelector('.mobile_navbar_div');
+    const mobile_navbar  = document.querySelector('.mobile_navbar');
+    menu.addEventListener('click',()=>{
+       mobile_navbar_div.classList.toggle('mobile_navbar_active');
+       mobile_navbar.classList.toggle('mobile_navbar_items_active');
+    });
+}
+
+function displaySize(){
+    var res = window.innerWidth + "x" + window.innerHeight;
+    document.getElementById("result").innerHTML = res;
+
+}
+
+
+displaySize();
+navSlide();
+
+
